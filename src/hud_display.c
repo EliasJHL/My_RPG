5fe92eb951e287fd7465b65be0f3b8cd61c22b5b
Elias Josué HAJJAR LLAUQUEN <145333474+EliasJHL@users.noreply.github.com>
2024-05-13 05:13:24 +0200

    [ADD] player display on inventory menu

--- src/hud_display.c
+++ src/hud_display.c
@@ -54,12 +54,19 @@ void inventory_menu(data_t *data)
     sfVector2f bg_pos = {center.x - 1920 / 2, center.y - 1080 / 2};
     sfVector2f s = {(656 * 0.7), (544 * 0.9)};
     sfVector2f sprite_pos = {center.x - s.x / 2, center.y - s.y / 2};
+    sfVector2f player_pos = {center.x - 180, center.y - 199};
 
+    sfSprite_setScale(data->player->player_sprite, (sfVector2f){3, 3});
+    sfSprite_setPosition(data->player->player_sprite, player_pos);
     sfRectangleShape_setPosition(data->inv->inv, bg_pos);
     sfSprite_setPosition(data->inv->inv_sprite, sprite_pos);
     sfSprite_setScale(data->inv->inv_sprite, (sfVector2f){0.7, 0.9});
     sfRenderWindow_drawRectangleShape(data->window, data->inv->inv, NULL);
     sfRenderWindow_drawSprite(data->window, data->inv->inv_sprite, NULL);
+    sfRenderWindow_drawSprite(data->window, data->player->player_sprite, NULL);
+    sfSprite_setPosition(data->player->player_sprite,
+        data->player->player_pos);
+    sfSprite_setScale(data->player->player_sprite, (sfVector2f){1, 1});
     display_slots(data);
 }
 
