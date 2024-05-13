a3147a197cef01edede54921f3b73e53b46260d7
Elias Josué HAJJAR LLAUQUEN <145333474+EliasJHL@users.noreply.github.com>
2024-05-13 04:21:42 +0200

    [FIX POS] position of the notification fixed

--- src/notification_system.c
+++ src/notification_system.c
@@ -7,35 +7,31 @@
 
 #include "../include/my.h"
 
-int cooldown_notif(data_t *data, sfVector2f pos, sfVector2f center)
+void cooldown_notif(data_t *data, sfVector2f pos, sfVector2f center)
 {
     double seconds;
     static int i = 0;
-    sfClock *ck = sfClock_create();
-    sfTime time = sfClock_getElapsedTime(ck);
-    double time_elapsed = sfTime_asSeconds(time);
 
     data->notif->elapsed_time = sfClock_getElapsedTime(data->notif->clock);
     seconds = sfTime_asSeconds(data->notif->elapsed_time);
-    if (seconds >= 0 && seconds <= 2 && time_elapsed >= 0.15)
-        i += 1;
-    if (seconds >= 6 && seconds <= 8 && time_elapsed >= 0.15)
-        i -= 1;
-    if (seconds >= 8) {
+    pos.x -= i;
+    if (pos.x > center.x + 300)
+        i += 3;
+    if (seconds >= 5) {
         i = 0;
         data->notif->active = false;
         data->notif->content = 0;
         sfClock_restart(data->notif->clock);
     }
-    sfClock_restart(ck);
-    return i;
+    sfSprite_setPosition(data->notif->notif, pos);
+    sfText_setPosition(data->notif->text,
+        (sfVector2f){pos.x + 40, pos.y + 38});
 }
 
-/*pos.x -= */
 void notification_display(data_t *data)
 {
     sfVector2f center = sfView_getCenter(data->player->camera);
-    sfVector2f pos = {center.x + 300, center.y - 200};
+    sfVector2f pos = {center.x + 400, center.y - 200};
 
     sfSprite_setScale(data->notif->notif, (sfVector2f){0.8, 0.8});
     sfText_setCharacterSize(data->notif->text, 40);
@@ -43,9 +39,6 @@ void notification_display(data_t *data)
     sfText_setString(data->notif->text, data->notif->content);
     sfText_setColor(data->notif->text, sfBlack);
     cooldown_notif(data, pos, center);
-    sfSprite_setPosition(data->notif->notif, pos);
-    sfText_setPosition(data->notif->text,
-        (sfVector2f){pos.x + 40, pos.y + 38});
     sfRenderWindow_drawSprite(data->window, data->notif->notif, NULL);
     sfRenderWindow_drawText(data->window, data->notif->text, NULL);
 }
