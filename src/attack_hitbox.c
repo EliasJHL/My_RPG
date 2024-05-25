2016dc166c7593345328454e20b85a944830dcd3
Enzo <enzo.lobato-coutinho@epitech.eu>
2024-05-25 15:23:33 +0200

    [ADD]xp_add function when slime killed

--- src/attack_hitbox.c
+++ src/attack_hitbox.c
@@ -17,6 +17,7 @@ static void check_hit_slime(data_t *data, int i)
     if (sfFloatRect_intersects(&rect1, &rect2, &intersection)) {
         data->ennemies->slime[i]->life -= 10;
         if (data->ennemies->slime[i]->life <= 0) {
+            xp_add(3, data);
             data->ennemies->slime[i]->is_alive = false;
         }
         if (data->player->direction == 0)
